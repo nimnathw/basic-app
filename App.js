794ec44010db9bce7330cc79c6b218a752620cf7
nimnathw <nimnathw@gmail.com>
2023-01-29 09:56:56 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
+
+const App = () => {
+  const [prices, setPrices] = useState([]);
+  const [showPrices, setShowPrices] = useState(false);
+
+  const handlePress = () => {
+    setShowPrices(!showPrices);
+    fetch('https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=14&interval=daily')
+    .then(response => response.json())
+    .then(json => {
+        if (json.prices) {
+          setPrices(json.prices.map(price => price[1]));
+        } else {
+          console.log("Data or prices key not found in API response");
+        }
+    })
+    .catch(error => {
+        console.log(error);
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      { showPrices && prices.map((price, index) => (
+        <Text key={index}> {`${index + 1}. ${price}`}</Text>
+      ))}
+      <Button title="Fetch Prices" onPress={handlePress} />
+    </View>
+  );
+};
+
+export default App;
